e5989c8ab5abdb4ecc0debea0be0d588191bf159
khairizalkhalid <khairizalkhalid@outlook.com>
2025-04-22 21:59:37 +0800

    init hello world using tokio axum.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("Start listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<div>Hello World</div>")
+}
