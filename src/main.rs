784d0c073cc091cb16cdd5b4c0ce823c804bb21d
khairizalkhalid <khairizalkhalid@outlook.com>
2025-04-29 21:51:20 +0800

    add sample implementation get hello world with a param "opt"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use axum::{Json, Router, routing::get};
+use std::i8;
+
+use axum::{Json, Router, extract::Path, routing::get};
 use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello-world", get(hello_world));
+    let app = Router::new()
+        .route("/hello-world", get(hello_world))
+        .route("/hello-world/{opt}", get(hello_world_options));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -23,5 +27,21 @@ async fn hello_world() -> Json<HelloWorlResp> {
     let resp = HelloWorlResp {
         message: "Hello World".to_string(),
     };
+    Json(resp)
+}
+
+async fn hello_world_options(Path(opt): Path<i8>) -> Json<HelloWorlResp> {
+    let resp = match opt {
+        0 => HelloWorlResp {
+            message: "ゼロ".to_string(),
+        },
+        1 => HelloWorlResp {
+            message: "Itchy".to_string(),
+        },
+        _ => HelloWorlResp {
+            message: "-_-".to_string(),
+        },
+    };
+
     Json(resp)
 }
